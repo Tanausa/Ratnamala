66f5df9a5ffeda74e73bc71edde52cc21f671414
Charvik <Charvik@LAPTOP-S0IS0QVR>
2022-03-26 09:19:32 +0530

    ! commint to repo git

---
+++ src/PPrao.java
@@ -0,0 +1,9 @@
+
+public class PPrao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Veny_Mama.java
@@ -0,0 +1,9 @@
+
+public class Veny_Mama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
